0e7425bc1f9ec58d41f305e8ddffa7bba1881193
HELLAS <HELLAS@HELLAS.mshome.net>
2020-03-27 18:05:55 -0400

    Second Commit

---
+++ CRUD-user-SpringBoot/src/main/java/com/springboot/themplate/CrudUserSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.themplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudUserSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudUserSpringBootApplication.class, args);
+	}
+
+}
